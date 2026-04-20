aa6eb12aea95b29c6a790c89e0a40519d36adb18
user <user@host.docker.internal>
2026-04-20 14:04:53 +0900

    FirstCommit

---
+++ src/User.cpp
@@ -0,0 +1,18 @@
+#include <board.h>
+
+void setup() {
+}
+
+void loop() {
+    LcdDrv_clear();
+    LcdDrv_setCursor(0, 0);
+    LcdDrv_print("Hello");
+    LcdDrv_update();
+    delay(1000);
+
+    LcdDrv_clear();
+    LcdDrv_setCursor(1, 0);
+    LcdDrv_print("World");
+    LcdDrv_update();
+    delay(1000);
+}
